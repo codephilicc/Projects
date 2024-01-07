e0252dafd4320bfe2dee592bac02de0433410730
Rahul <rahulkr.vk@gmail.com>
2024-01-07 16:35:49 +0530

    to_do_list2

--- To_do_list_ConsoleApp/src/To_do_list.java
+++ To_do_list_ConsoleApp/src/To_do_list.java
@@ -1,8 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class To_do_list {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<String> list = new ArrayList<String>();
+		
+		Scanner sc = new Scanner(System.in);
+//		String Todos;
+		String addTodos, deleteTodos;
+
+		while (true) {
+
+			System.out.println("Welcome todo_List..");
+			System.out.println("To add todos, please press 1: ");
+			System.out.println("To delete todos,please press 2: ");
+			System.out.println("To exit from todos,please press 3: ");
+
+			int i = sc.nextInt();
+
+			switch (i) {
+
+			case 1:
+				System.out.println("Please add task: ");
+				addTodos = sc.next();
+				System.out.println(addTodos);
+				list.add(addTodos);
+				System.out.println(list);
+				break;
+			case 2:
+				System.out.println("Please delete task: ");
+				deleteTodos = sc.next();
+				
+				break;
+			case 3:
+				System.exit(0);
+
+			}
+
+		}
 
 	}
 
