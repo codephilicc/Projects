d9cf3f90f395b4047090182c14fde7e9dadf3e6c
Rahul <rahulkr.vk@gmail.com>
2024-01-14 22:06:36 +0530

    update

--- To_do_list_ConsoleApp/src/To_do_list.java
+++ To_do_list_ConsoleApp/src/To_do_list.java
@@ -1,18 +1,21 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class To_do_list {
 
 	public static void main(String[] args) {
-		ArrayList<String> list = new ArrayList<String>();
-		
+
+		ArrayList<String> list = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
+		
 //		String Todos;
 		String addTodos, deleteTodos;
 
 		while (true) {
-
-			System.out.println("Welcome todo_List..");
+			if (list.isEmpty()) {
+				System.out.println("Welcome todo_List..");
+			}
 			System.out.println("To add todos, please press 1: ");
 			System.out.println("To delete todos,please press 2: ");
 			System.out.println("To exit from todos,please press 3: ");
@@ -23,29 +26,47 @@ public class To_do_list {
 
 			case 1:
 				System.out.println("Please add task: ");
-				addTodos = sc.nextLine();
-				System.out.println(addTodos);
+				addTodos = sc.next();
+//				addTodo(addTodos);
+				System.out.println("Task to add "+addTodos);
 				list.add(addTodos);
-				System.out.println(list);
+				list.forEach(x -> System.out.print(x + " "));
+				System.out.println();
+//				System.out.println(list);
 				break;
 			case 2:
 				System.out.println("Please delete task: ");
-				deleteTodos = sc.nextLine();
-				System.out.println(deleteTodos);
-				if(list.contains(deleteTodos)) {
-					list.remove(deleteTodos);
-				}else {
-					System.out.println("item is not present");
+				deleteTodos = sc.next();
+				System.out.println("Task to delete "+deleteTodos);
+//				deleteTodo(deleteTodos);
+				if (list.contains(deleteTodos)) {
+					if (!list.isEmpty()) {
+						list.remove(deleteTodos);
+						list.forEach(x -> System.out.print(x + " "));
+						System.out.println();
+					} else {
+						System.out.println("no task available");
+					}
+				} else {
+					System.out.println("no such task is there");
 				}
-				System.out.println(list);
 				break;
 			case 3:
 				System.exit(0);
 
 			}
-
+			 
 		}
-
 	}
 
+//	public static void addTodo(String addTodos) {
+//		System.out.println(addTodos);
+//		list.add(addTodos);
+//		return list;
+//	}
+//	public static void deleteTodo(String deleTodos){
+//		System.out.println(deleTodos);
+//		list.remove(deleTodos);
+//		return list;
+//	}
 }
